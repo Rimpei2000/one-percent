a00a947db7ae59a662eb795a82f4a526accd0f75
Rimpei <70897624+Rimpei2000@users.noreply.github.com>
2025-01-02 08:21:39 -0800

    fix error

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,9 @@
 import { AuthTabs } from "@/components/AuthTabs";
 
-type Props = {
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-};
-
-export default async function LoginPage({ searchParams }: Props) {
-  const currSearchParams = await searchParams;
-  const message = currSearchParams.message as string;
-
+export default async function LoginPage() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <AuthTabs message={message} />
+      <AuthTabs />
     </div>
   );
 }
